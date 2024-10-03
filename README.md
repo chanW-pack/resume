8aa072ec0307e901c1ceb3dcf4518f451586c72d
chanW-pack <chanwoo9730@naver.com>
2024-10-03 08:39:19 +0000

    Initial setting

---
+++ README.md
@@ -0,0 +1,6 @@
+🔵 resume.devcw.site
+Netlify Status
+
+개발자 박찬우 기술 이력서
+
+Theme UI
---
+++ payload/profile.ts
@@ -0,0 +1,44 @@
+import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook, faGithub } from '@fortawesome/free-brands-svg-icons';
+
+import { faBell } from '@fortawesome/free-regular-svg-icons';
+import { IProfile } from '../component/profile/IProfile';
+import image from '../asset/_sample.jpg';
+
+const profile: IProfile.Payload = {
+  disable: false,
+
+  // image: 'https://resume.yowu.dev/static/image/profile_2019.png',
+  image,
+  name: {
+    title: '박찬우',
+    small: '(Woo DEV)',
+  },
+  contact: [
+    {
+      title: 'chenwoo9730@gmail.com',
+      link: 'mailto:tux@linux.org',
+      icon: faEnvelope,
+    },
+    {
+      title: 'Please contact me by email',
+      icon: faPhone,
+      badge: true,
+    },
+    {
+      link: 'https://github.com/chanW-pack',
+      icon: faGithub,
+    },
+    {
+      link: 'https://cwpack0730.tistory.com/',
+      icon: faBell,
+    },
+  ],
+  notice: {
+    title:
+      "The content below is all fictitious and is just a sample from 'https://github.com/uyu423/resume-nextjs'.",
+    icon: faBell,
+  },
+};
+
+export default profile;
